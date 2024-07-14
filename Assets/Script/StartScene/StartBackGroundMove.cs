75a976d44f8cf925b61e4715af4162e03baf125f
RAYTHARY\ghfjt <ghfjtorl121291@naver.com>
2024-07-14 13:21:30 +0900

    no message

--- Assets/Script/StartScene/StartBackGroundMove.cs
+++ Assets/Script/StartScene/StartBackGroundMove.cs
@@ -10,11 +10,13 @@ public class StartBackGroundMove : MonoBehaviour
 
 
     [SerializeField] private Image imgSign;
+    [SerializeField] private TextMeshProUGUI textTitle;
     private TextMeshPro m_textMesh;
     private Transform m_backGroudTrs;
     private Vector3 m_targetVec;
-    private bool m_firstStart = false;
+
     private bool m_firstCheck = false;
+    private bool m_moveEnd = false;
     private bool m_signColorCheck = false;
     [SerializeField] private float moveSpeed = 1;
     void Start()
@@ -35,46 +37,47 @@ public class StartBackGroundMove : MonoBehaviour
 
     private void anyKeyCheck()
     {
-        if (m_firstCheck == false)
-        {
-            if (m_firstStart == false)
-            {
-                if (Input.anyKeyDown)
-                {
-                    m_firstStart = true;
-                    m_textMesh.gameObject.SetActive(false);
-                }
-            }
 
+        if (m_firstCheck == false && m_moveEnd == false)
+        {
+            if (Input.anyKeyDown)
+            {
+                m_firstCheck = true;
+                m_textMesh.gameObject.SetActive(false);
+            }
         }
+
     }
     private void backGroundMove()
     {
-        if (m_firstStart)
+        if (m_firstCheck)
         {
             m_backGroudTrs.position = Vector2.MoveTowards(m_backGroudTrs.position, m_targetVec, moveSpeed * Time.deltaTime);
             if (m_backGroudTrs.position.y <= m_targetVec.y)
             {
-                m_firstCheck = true;
+                m_moveEnd = true;
+                m_firstCheck = false;
             }
         }
     }
 
     private void SeeSignAndTitle()
     {
-        if (m_firstCheck)
+        if (m_moveEnd && m_signColorCheck == false)
         {
-            if (m_signColorCheck == false)
-            {
-                Color signColor = imgSign.color;
-                signColor.a += Time.deltaTime * 0.5f;
-                imgSign.color = signColor;
-                if (signColor.a > 1)
-                {
-                    m_signColorCheck = true;
-                }
-            }
 
+            Color signColor = imgSign.color;
+            signColor.a += Time.deltaTime * 0.5f;
+            Color titleColor = textTitle.color;
+            titleColor.a += Time.deltaTime * 0.5f;
+
+            textTitle.color = titleColor;
+            imgSign.color = signColor;
+
+            if (signColor.a >= 1 && titleColor.a >= 1)
+            {
+                m_signColorCheck = true;
+            }
         }
     }
 
